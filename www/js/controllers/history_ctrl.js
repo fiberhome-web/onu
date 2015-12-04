e3d0256d10483de93fae954907102f8a6ac1ba2b
shilin <shilin@fiberhome.com>
2015-12-04 08:54:50 +0800

    新增批量删除功能

--- www/js/controllers/history_ctrl.js
+++ www/js/controllers/history_ctrl.js
@@ -1,7 +1,7 @@
 
 angular.module('starter.controllers') 
 	.controller('HistoryCtrl', function($scope,$log, $ionicGesture, $ionicLoading,
-		$ionicActionSheet, $ionicListDelegate) {
+		$ionicActionSheet, $ionicListDelegate, $ionicModal) {
 
 		//下拉刷新数据
 		$scope.doRefresh = function(){
@@ -26,11 +26,53 @@ angular.module('starter.controllers')
 	    $scope.type = 1;
 
 	    $scope.filterType = function(type){
-	    	debugger;
+	  
 	    	$scope.type = type;
 	    }
 
+	    $ionicModal.fromTemplateUrl('my-modal.html', {
+		    scope: $scope,
+		    animation: 'slide-in-up'
+		  }).then(function(modal) {
+		    $scope.modal = modal;
+		  });
+		  $scope.openModal = function() {
+		    $scope.modal.show();
+		  };
+
+		  $scope.batchDelele = function(){
+		  	$scope.modal.hide();
+		  	$scope.shouldShowCheckbox = true;
+		  }
+
+		  $scope.hideDelete = function(){
+		  	$scope.shouldShowCheckbox = false;
+		  }
+
+		  
+
 	    $scope. list = [{name : '万科魅力之城 万科魅力之城  ', date : '2015-11-11 11:11:11', status : 1, id : '001'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				     {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'}, {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
+				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
 				    {name : '万科魅力之城 万科魅力之城 万科魅力之城 ', date : '2015-11-11 11:11:11', status : 0, id : '002'},
 				   
 				    {name : '万科魅力之城 万科魅力之城22333  ', date : '2015-11-11 11:11:11', status : 1, id : '003'},
