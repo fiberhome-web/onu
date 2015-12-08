6241876343cc2ccf62f5180b231e29293977df83
zhuzhuxia556868 <763622570@qq.com>
2015-12-08 09:35:12 +0800

    解决键盘隐藏时全屏问题

--- www/js/app.js
+++ www/js/app.js
@@ -15,16 +15,21 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','n
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
 	
-	
+    // 键盘隐藏事件，当键盘隐藏时触发。设置app全屏显示，输入框输入完成之后键盘隐藏，
+    // 此时未知原因导致ionic.Platform.isFullScreen变为false，app不再是全屏状态导致界面异常。
+    // 此处使用键盘隐藏事件在每次键盘隐藏时都重设app为全屏显示。
+    window.addEventListener('native.keyboardhide', function(e){
+      ionic.Platform.fullScreen(true);
+    });
 	
   });
+
 })
 
 .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
