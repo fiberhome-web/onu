3a83cfe4fadc97ce3c51ca00462f265ffc8491c7
shilin <shilin@fiberhome.com>
2015-12-17 14:45:25 +0800

    _dev文件夹更名为mock

--- www/js/controllers/basic_ctrl.js
+++ www/js/controllers/basic_ctrl.js
@@ -1,17 +1,21 @@
-
 angular.module('starter.controllers')
-	.controller('BasicCtrl', function($scope,$state,$http,Const) {
+    .controller('BasicCtrl', function($scope, $state, $http, Const) {
 
-		$scope.checking = function(){
-			$state.go('tab.check',{checkStatus : 0})
-		}
+        $scope.checking = function() {
+            $state.go('tab.check', {
+                checkStatus: 0
+            })
+        }
 
-		var url = Const.getReqUrl();
-		var command = {'command' : 'getDeviceInfo' };
-		$http.post(url,command).success(function(res){
-			var deviceInfo = res;
-		}).error(function(data, status){
-			alert('data:' + data + '\n' + 'status:' + status + '\n');
-		});
-   
-});
+        var url = Const.getReqUrl();
+        var command = {
+            'command': 'getDeviceInfo'
+        };
+        $http.post(url, command).success(function(res) {
+            debugger;
+            var deviceInfo = res;
+        }).error(function(data, status) {
+            alert('data:' + data + '\n' + 'status:' + status + '\n');
+        });
+
+    });
