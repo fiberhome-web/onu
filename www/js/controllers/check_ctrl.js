4c0de00d114e0c8ffa451785301b5b88b183687f
zhuzhuxia556868 <763622570@qq.com>
2015-12-17 14:58:51 +0800

    格式化tab-check.html, check_ctrl.js文件

--- www/js/controllers/check_ctrl.js
+++ www/js/controllers/check_ctrl.js
@@ -1,52 +1,50 @@
-
 angular.module('starter.controllers')
-	.controller('CheckCtrl', function($scope,$state,$http,$stateParams,$ionicPopup) {
+    .controller('CheckCtrl', function($scope, $state, $http, $stateParams, $ionicPopup) {
 
-		//检查checkStatus 0 ：检查全部项
-		var checkStatus = $stateParams.checkStatus;
-
-
-		$scope.remove = function(chat){
-		
-		}
-
-		$scope.checkPon = function(){
-			$scope.check1 = true;
-		}
+        //检查checkStatus 0 ：检查全部项
+        var checkStatus = $stateParams.checkStatus;
 
 
-		$scope.localInfo = {
-			status : '检测结果'
-		}
+        $scope.remove = function(chat) {
 
-		$scope.r_status = "0";
+        }
 
-		$scope.showPopup = function() {
-		   $scope.data = {}
+        $scope.checkPon = function() {
+            $scope.check1 = true;
+        }
 
-		   // 一个精心制作的自定义弹窗
-		   var myPopup = $ionicPopup.show({
-		    // template: '<input type="text" ng-model="data.wifi">{{localInfo.status}}',
-		     templateUrl : 'popup.html',
-		     title: '请输入报告信息',
-		     cssClass : 'reportPopup',
-		     scope: $scope,
-		     buttons: [
-		       { text: 'Cancel' },
-		       {
-		         text: '<b>Save</b>',
-		         type: 'button-positive',
-		         onTap: function(e) {
-		           if (!$scope.data.wifi) {
-		             //不允许用户关闭，除非他键入wifi密码
-		             e.preventDefault();
-		           } else {
-		             return $scope.data.wifi;
-		           }
-		         }
-		       },
-		     ]
-		   });
-		}
-	
-});
+
+        $scope.localInfo = {
+            status: '检测结果'
+        }
+
+        $scope.r_status = "0";
+
+        $scope.showPopup = function() {
+            $scope.data = {}
+
+            // 一个精心制作的自定义弹窗
+            var myPopup = $ionicPopup.show({
+                // template: '<input type="text" ng-model="data.wifi">{{localInfo.status}}',
+                templateUrl: 'popup.html',
+                title: '请输入报告信息',
+                cssClass: 'reportPopup',
+                scope: $scope,
+                buttons: [{
+                    text: 'Cancel'
+                }, {
+                    text: '<b>Save</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if (!$scope.data.wifi) {
+                            //不允许用户关闭，除非他键入wifi密码
+                            e.preventDefault();
+                        } else {
+                            return $scope.data.wifi;
+                        }
+                    }
+                }, ]
+            });
+        }
+
+    });
