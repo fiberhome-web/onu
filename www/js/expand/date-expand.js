d5d9676363b4e3c6ceb5bdc6dbd10bc48923e647
shilin <shilin@fiberhome.com>
2015-12-21 10:56:30 +0800

    增加数组扩展

--- www/js/expand/date-expand.js
+++ www/js/expand/date-expand.js
@@ -27,22 +27,3 @@ Date.prototype.format = function(fmt) {
 }
 
 
-
-/*
- *  方法:Array.remove(dx)
- *  功能:根据元素位置值删除数组元素.
- *  参数:元素值
- *  返回:在原数组上修改数组
- *  作者：pxp
- */
-Array.prototype.remove = function(dx) {
-    if (isNaN(dx) || dx > this.length) {
-        return false;
-    }
-    for (var i = 0, n = 0; i < this.length; i++) {
-        if (this[i] != this[dx]) {
-            this[n++] = this[i];
-        }
-    }
-    this.length -= 1;
-};
