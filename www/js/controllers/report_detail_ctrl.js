61ee0dc48bc42b3d2f3e6b95006a25eaf78cb7e5
shilin <shilin@fiberhome.com>
2015-12-28 17:31:55 +0800

    生成报告文件

--- www/js/controllers/report_detail_ctrl.js
+++ www/js/controllers/report_detail_ctrl.js
@@ -1,65 +1,83 @@
-
+'use strict';
 angular.module('starter.controllers')
-	.controller('ReportDetailCtrl', function($scope,$state,$http,$ionicHistory,
-		$stateParams, DB, $ionicPopup, $ionicModal, $ionicPopover) {
+    .controller('ReportDetailCtrl', function($scope, $state, $http, $ionicHistory,
+        $stateParams, DB, $ionicPopup, $ionicModal, $ionicPopover, File) {
 
-		$scope.reportLocal = ONU_LOCAL.report;
+        $scope.reportLocal = ONU_LOCAL.report;
 
-		var id = $stateParams.reportId;
+        //获取report id
+        var id = $stateParams.reportId;
 
-		$scope.back = function(){
-			$ionicHistory.goBack();
-		}
-
-
-		DB.queryById(id).then(function(res){
-			var item = res.rows.item(0);
-			$scope.report = item;
-			$scope.detail = JSON.parse(item.data);
-		});
-
-		
-		//document.getElementById('report').innerHTML
-
-		
-		// .fromTemplateUrl() 方法
-		$ionicPopover.fromTemplateUrl('my-popover.html', {
-		   scope: $scope,
-		}).then(function(popover) {
-		   $scope.popover = popover;
-		});
+        //登录按钮事件
+        $scope.back = function() {
+            $ionicHistory.goBack();
+        };
 
 
-		$scope.openPopover = function($event) {
-		   $scope.popover.show($event);
-		};
+        //根据查询报告并渲染数据
+        DB.queryById(id).then(function(res) {
+            var item = res.rows.item(0);
+            $scope.report = item;
+            $scope.detail = JSON.parse(item.data);
+
+            //查看报告文件是否生成过，没有则生成报告
+            File.checkFile(item.name).then(function() {}, function(error) {
+                //如果code为1说明没有找到文件，则要生成报告文件
+                if (error.code === 1) {
+                	//延时1s生成报告，防止angular没有将页面没有渲染完成
+                    setTimeout(function() {
+                        //创建报告
+                        File.createReport(item.name, document.getElementById('report').innerHTML);
+                    }, 1000);
+                }
+            });
+
+        });
 
 
 
-		$scope.showOpDiv = function(){
-			var myPopup = $ionicPopup.show({
-		     template: '<input type="password" ng-model="data.wifi">',
-		     title: 'Enter Wi-Fi Password',
-		     subTitle: 'Please use normal things',
-		     scope: $scope,
-		     buttons: [
-		       { text: 'Cancel' },
-		       {
-		         text: '<b>Save</b>',
-		         type: 'button-positive',
-		         onTap: function(e) {
-		           if (!$scope.data.wifi) {
-		             //不允许用户关闭，除非他键入wifi密码
-		             e.preventDefault();
-		           } else {
-		             return $scope.data.wifi;
-		           }
-		         }
-		       },
-		     ]
-		   });
-		}
 
 
-		 
-});
+        //document.getElementById('report').innerHTML
+
+
+        // .fromTemplateUrl() 方法
+        $ionicPopover.fromTemplateUrl('my-popover.html', {
+            scope: $scope,
+        }).then(function(popover) {
+            $scope.popover = popover;
+        });
+
+
+        $scope.openPopover = function($event) {
+            $scope.popover.show($event);
+        };
+
+
+
+        $scope.showOpDiv = function() {
+            var myPopup = $ionicPopup.show({
+                template: '<input type="password" ng-model="data.wifi">',
+                title: 'Enter Wi-Fi Password',
+                subTitle: 'Please use normal things',
+                scope: $scope,
+                buttons: [{
+                    text: 'Cancel'
+                }, {
+                    text: '<b>Save</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if (!$scope.data.wifi) {
+                            //不允许用户关闭，除非他键入wifi密码
+                            e.preventDefault();
+                        } else {
+                            return $scope.data.wifi;
+                        }
+                    }
+                }, ]
+            });
+        };
+
+
+
+    });
--- www/js/services/file_service.js
+++ www/js/services/file_service.js
@@ -1,85 +1,96 @@
-angular.module('starter.services').service('File',function($log, $cordovaFile,
-	$filter, $ionicPlatform){
+'use strict';
+angular.module('starter.services').service('File', function($log, $cordovaFile,
+    $filter, $ionicPlatform) {
 
-	var fileSystem;
+    var fileSystem;
 
-	//存放报告的文件夹名称
-	var reportDir = 'onu_report';
-	//删除的报告存放的文件夹
-	var deleteDir = 'onu_del';
-	var _reportDir = reportDir + '/';
-	var _deleteDir = deleteDir + '/';
-
-	
-	//  在File service初始化的时候:
-	//  1.检查onu_report是否存在，不存在则建立onu_report文件夹
-	//  2.清空onu_del文件夹
-	$ionicPlatform.ready(function() {
-	  	//fileSystem = cordova.file.externalRootDirectory;
-
-	  	//检查存放报告的文件夹以及用于删除文件的文件夹是否存在，不存在则创建
-	  	$cordovaFile.checkDir(fileSystem, reportDir).then(success ,function(error){
-			//如果存放报告的文件夹不存在，重新新建
-			if(error.code === 1) {
-				$cordovaFile.createDir(fileSystem, reportDir, false).then(success, function(error){
-					alert("onu_report can't be crated,error message is : " + error.message);
-				});
-			} else {
-				alert("onu_report can't be crated,error message is : " + error.message);
-			}
-
-		});
+    //存放报告的文件夹名称
+    var reportDir = 'onu_report';
+    //文件类型
+    var fileType = '.html';
+    //删除的报告存放的文件夹
+    var deleteDir = 'onu_del';
+    var _reportDir = reportDir + '/';
+    var _deleteDir = deleteDir + '/';
 
 
-		//清空onu_del的所有内容，重新新建onu_del文件夹
-		$cordovaFile.removeRecursively(fileSystem, deleteDir).then(function(){
-			$cordovaFile.createDir(fileSystem, deleteDir, true).then(success, function(error){
-				alert("onu_del can't be crated,error message is : " + error.message);
-			});
-		},function(error){
-			//如果是错误1表示文件夹不存在，则重新创建
-			if(error.code === 1) {
-				$cordovaFile.createDir(fileSystem, deleteDir, true).then(success, function(error){
-					alert("onu_del can't be crated,error message is : " + error.message);
-				});
-			}else {
-				alert('error :' + JSON.stringify(error));
-			}
-		});
-		
-	});
+    //  在File service初始化的时候:
+    //  1.检查onu_report是否存在，不存在则建立onu_report文件夹
+    //  2.清空onu_del文件夹
+    $ionicPlatform.ready(function() {
+        fileSystem = cordova.file.externalRootDirectory;
 
-	//成功回调
-	function success(success){
-		alert('success :' + JSON.stringify(success));
-	}
-	//失败回调
-	function error(error) { 
-		alert('error :' + JSON.stringify(error));
-	}
+        //检查存放报告的文件夹以及用于删除文件的文件夹是否存在，不存在则创建
+        $cordovaFile.checkDir(fileSystem, reportDir).then(success, function(error) {
+            //如果存放报告的文件夹不存在，重新新建
+            if (error.code === 1) {
+                $cordovaFile.createDir(fileSystem, reportDir, false).then(success, function(error) {
+                    alert('onu_report can\'t be crated,error message is : ' + error.message);
+                });
+            } else {
+                alert('onu_report can\'t be crated,error message is :'  + error.message);
+            }
 
-	//创建报告
-	this.createReport = function(fileName, data){
-		$cordovaFile.writeFile(fileSystem , _reportDir + fileName, data, true).then(success,error);
-	}
+        });
 
-	//增加一条记录
-	this.addReportRecord = function(fileName, content){
-		$cordovaFile.writeExistingFile(fileSystem, _reportDir + fileName, content).then(success,error);
-	}
 
-	//读取文件
-	this.readReport = function(fileName){
-		return $cordovaFile.readAsText(fileSystem, _reportDir + fileName);
-	}
+        //清空onu_del的所有内容，重新新建onu_del文件夹
+        $cordovaFile.removeRecursively(fileSystem, deleteDir).then(function() {
+            $cordovaFile.createDir(fileSystem, deleteDir, true).then(success, function(error) {
+                alert('onu_del can\'t be crated,error message is : ' + error.message);
+            });
+        }, function(error) {
+            //如果是错误1表示文件夹不存在，则重新创建
+            if (error.code === 1) {
+                $cordovaFile.createDir(fileSystem, deleteDir, true).then(success, function(error) {
+                    alert('onu_del can\'t be crated,error message is : ' + error.message);
+                });
+            } else {
+                alert('error :' + JSON.stringify(error));
+            }
+        });
 
-	//移动报告文件到删除文件夹
-	this.moveFile = function(fileName){
-		$cordovaFile.moveFile(fileSystem, _reportDir + fileName,fileSystem, _deleteDir + fileName).then(success,error);
-	}
+    });
 
-	//删除文件夹及里面的所有文件
-	this.removeRecursively = function(){
-		$cordovaFile.removeRecursively(fileSystem,'onu_del').then(success,error);
-	}
+    //成功回调
+    function success(info) {
+        console.info('success :' + JSON.stringify(info));
+    }
+    //失败回调
+    function error(info) {
+        alert('error :' + JSON.stringify(info));
+    }
+
+    //创建报告
+    this.createReport = function(fileName, data) {
+    	//增加报告文件基本的HTML格式代码
+    	var head = '<!DOCTYPE html><html><head><meta charset="utf-8"></head>';
+    	var tail = '</html>';
+        $cordovaFile.writeFile(fileSystem, _reportDir + fileName + fileType, head + data + tail, true).then(success, error);
+    };
+
+    //增加一条记录
+    this.addReportRecord = function(fileName, content) {
+        $cordovaFile.writeExistingFile(fileSystem, _reportDir + fileName + fileType, content).then(success, error);
+    };
+
+    //读取文件
+    this.readReport = function(fileName) {
+        return $cordovaFile.readAsText(fileSystem, _reportDir + fileName + fileType);
+    };
+
+    //移动报告文件到删除文件夹
+    this.moveFile = function(fileName) {
+        $cordovaFile.moveFile(fileSystem, _reportDir + fileName + fileType, fileSystem, _deleteDir + fileName + fileType).then(success, error);
+    };
+
+    //删除文件夹及里面的所有文件
+    this.removeRecursively = function() {
+        $cordovaFile.removeRecursively(fileSystem, deleteDir).then(success, error);
+    };
+
+    //检查文件是否存在
+    this.checkFile = function(fileName){
+    	return $cordovaFile.checkFile(fileSystem, _reportDir + fileName + fileType);
+    };
 });
