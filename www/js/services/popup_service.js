422d6bbe21151d7ca538a5f3d9f77a46766efdea
shilinmu <272169986@qq.com>
2016-02-25 16:33:03 +0800

    detail

--- www/js/services/popup_service.js
+++ www/js/services/popup_service.js
@@ -1,20 +1,23 @@
 angular.module('starter.services').service('Popup',['$ionicPopup','$rootScope', 
 	function($ionicPopup,$rootScope){
 
-	this.showPop = function(reason,msg){
+	this.showPop = function(option){
 
 		var html = '<div class="warn-tip">'+
                    '<div class="close-btn" ng-click="closePop()">'+
                    '<i class="iconfont">&#xe61d;</i></div>';
-                                
-                                          
-		if(reason) {
-			html = html + '<div>Reason:<br/>'+ reason + '</div>';                  
-		}
-		if(msg) {
-			html = html + '<div>Suggestion:<br/>'+ msg + '</div>';                  
-		}
 
+        if(option.note) {
+        	html = html + '<div>Note:<br/><br/>'+ option.note + '</div>'; 
+        } else {
+        	if(option.reason) {
+				html = html + '<div>Reason:<br/><br/>'+ option.reason + '</div>';                  
+			}
+			if(option.msg) {
+				html = html + '<div>Suggestion:<br/><br/>'+ option.msg + '</div>';                  
+			}
+        }
+                                                                      
 		html = html + '</div>';
 
 		var myPopup = $ionicPopup.show({
