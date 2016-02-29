52a2ba338e690814a020add25a8e466c09008235
shilinmu <272169986@qq.com>
2016-02-29 14:07:15 +0800

    icon bug fixed

--- www/js/controllers/report_detail_ctrl.js
+++ www/js/controllers/report_detail_ctrl.js
@@ -36,15 +36,19 @@ angular.module('starter.controllers')
             //记录文件地址
             filePath = 'file:///storage/emulated/0/onu_report/' + item.name + '.html';
 
+
+            //延时1s生成报告，防止angular没有将页面没有渲染完成
+            setTimeout(function() {
+                //创建报告
+                File.createReport(item.name, document.getElementById('report').innerHTML);
+            }, 1000);
+
             //查看报告文件是否生成过，没有则生成报告
             File.checkFile(item.name).then(function() {}, function(error) {
                 //如果code为1说明没有找到文件，则要生成报告文件
                 if (error.code === 1) {
-                    //延时1s生成报告，防止angular没有将页面没有渲染完成
-                    setTimeout(function() {
-                        //创建报告
-                        File.createReport(item.name, document.getElementById('report').innerHTML);
-                    }, 1000);
+                    
+                   
                 }
             });
         });
--- www/js/services/const_service.js
+++ www/js/services/const_service.js
@@ -1,5 +1,5 @@
 angular.module('starter.services').service('Const',function(){
-	this.reqUrl = 'http://192.168.1.1/app/';
+	this.reqUrl = 'http://192.168.1.1/app';
 
 	this.getReqUrl = function(){
 		return this.reqUrl;
